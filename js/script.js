c78720cc4b41d822410fa37caeadcb6b54820790
Denis Bondarenko <fvscorpion2@gmail.com>
2019-06-05 21:39:11 +0300

    added scroll animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function() {
+	$(window).scrollTop(0);
+
 	$('.toggles button').click(function() {
 		var get_id = this.id;
 		var get_current = $('.latest-projects .' + get_id);
@@ -25,5 +27,29 @@ $(function() {
 		var id  = $(this).attr('href'),
 		top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+	$(".section-head h2, .section-head h3, .section-head p").animated("zoomIn");
+
+	wow = new WOW(
+	{
+		mobile: false      
+	})
+	wow.init();
+
+	
+	$(".counters").waypoint(function() {
+		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+		$(".counter__number").each(function () {
+			var tcount = $(this).data("count");
+			$(this).animateNumber({
+				number: tcount,
+				easing: 'easeInQuad',
+				numberStep: comma_separator_number_step
+			},
+			1200);
+		});
+	}, {
+		offset : "80%"
 	});
 });
